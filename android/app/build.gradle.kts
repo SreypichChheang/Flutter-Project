35dc7010666bd14eaf7b94b106caf39c0dc0eb8c
PolliX123 <neath5738@gmail.com>
2025-06-24 18:26:00 +0700

    commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,9 +9,6 @@ android {
     namespace = "com.example.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"  // Fixed: Use specific NDK version required by plugins
-    namespace = "com.example.settingv2"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -25,10 +22,9 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.app"
-        applicationId = "com.example.settingv2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
